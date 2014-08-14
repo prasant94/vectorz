b672bf90b7651e2dc44994de9e2a499d3bd20842
Prasant Chidella <prasant12075@iiitd.ac.in>
2014-08-14 13:40:15 +0530

    tests to ensure input is not modified

--- src/test/java/mikera/matrixx/algo/TestLinear.java
+++ src/test/java/mikera/matrixx/algo/TestLinear.java
@@ -15,10 +15,17 @@ public class TestLinear {
     @Test 
     public void testSimpleSquareSolve() {
         AMatrix m= Matrix.create(new double[][] {{1,-2,1},{0,1,6},{0,0,1}});
+        AMatrix m_copy = Matrix.create(m);
         AMatrix mi=m.inverse();
         assertTrue(m.innerProduct(mi).isIdentity());
         
-        AVector x=Linear.solve(m, Vector.of(4,-1,2));
+        Vector b = Vector.of(4,-1,2);
+        Vector b_copy = Vector.create(b);
+        AVector x=Linear.solve(m, b);
+        
+//        input should not have been modified
+        assertEquals(b, b_copy);
+        assertEquals(m, m_copy);
         
         assertEquals(Vector.of(-24,-13,2),x);
     }
@@ -26,25 +33,49 @@ public class TestLinear {
     @Test 
     public void testSolveLeastSquaresVector() {
         AMatrix m= Matrix.create(new double[][] {{1,2},{3,4},{5,6}});
+        AMatrix m_copy = Matrix.create(m);
         
-        AVector x = Linear.solveLeastSquares(m, Vector.of(1,2,3));
-        
+        AVector b = Vector.of(1,2,3);
+        AVector b_copy = Vector.create(b);
+        AVector x = Linear.solveLeastSquares(m, b);
+
+//      input should not have been modified
+        assertEquals(b, b_copy);
+        assertEquals(m, m_copy);
+      
         assertEquals(Vector.of(0,0.5),x);
     }
     
     @Test 
     public void testSolveSquareVector() {
         AMatrix m= Matrix.create(new double[][] {{1,2,2},{1,4,1},{5,9,2}});
+        AMatrix m_copy = Matrix.create(m);
         
-        AVector x = Linear.solveLeastSquares(m, Vector.of(1,3,3));
+        AVector b = Vector.of(1,3,3);
+        AVector b_copy = Vector.create(b);
+        AVector x = Linear.solveLeastSquares(m, b);
+        
+//      input should not have been modified
+        assertEquals(b, b_copy);
+        assertEquals(m, m_copy);
+        
         assertTrue(Vector.of(-1.35294117647,1.05882352941,0.11764705882).epsilonEquals(x, 1e-8));
     }
     
     @Test 
     public void testSolveSquareMatrix() {
         AMatrix m= Matrix.create(new double[][] {{1,2,2},{1,4,1},{5,9,2}});
+        AMatrix m_copy = Matrix.create(m);
         
-        AMatrix x = Linear.solveLeastSquares(m, Matrix.create(new double[][]{{1},{3},{3}}));
+        AMatrix b = Matrix.create(new double[][]{{1},{3},{3}});
+        AMatrix b_copy = Matrix.create(b);
+        
+        AMatrix x = Linear.solveLeastSquares(m, b);
+        
+//      input should not have been modified
+        assertEquals(b, b_copy);
+        assertEquals(m, m_copy);
+        
         assertArrayEquals(new double[] {-1.35294117647,1.05882352941,0.11764705882},x.asDoubleArray(), 1e-8);
     }
     
@@ -58,7 +89,7 @@ public class TestLinear {
 
 	@Test
 	public void testSolveSquareMatrixSingular() {
-		AMatrix m= Matrix.create(new double[][] {{1,2,3},{4,5,6},{7,8,9}});
+	    AMatrix m = Matrix.create(new double[][] {{1,2,3},{4,5,6},{7,8,9}});
 		
 		assertNull(Linear.solve(m, Vector.of(1,3,3)));
 	}
